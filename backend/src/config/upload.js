e0fa59e36e29b375fd68dd0c0c3647834b0016ea
Lucas Castro <lucas.castro1993@hotmail.com>
2021-02-17 12:33:52 -0300

    indentando o codigo

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     storage: new multerr.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
-        filename: function(req, file, cb) {
+        filename: function (req, file, cb) {
             cb(null, file.originalname);
         }
     })
